5d5ecd979466830dbab758f0b0daeea1c82b45f9
avikdigidev <avikdigidev@gmail.com>
2022-04-26 02:21:18 +0530

    Top-Bottom approach Initial Commit- declared APIs

---
+++ src/com/lms/tester/Tester.java
@@ -0,0 +1,54 @@
+package com.lms.tester;
+
+import java.util.Date;
+import java.util.List;
+
+public class Tester {
+    public static void main(String[] args) {
+
+    }
+
+    public List<BookCopy> searchBooksByBookName(String bookName) {
+
+    }
+
+    public List<BookCopy> searchBooksByAuthorName(List<String> authorName) {
+
+    }
+
+    public List<BookCopy> searchBooksByBookId(int bookCopyId) {
+
+    }
+
+    public List<Member> searchMemberByMemberName(String memberName) {
+
+    }
+
+    public void addBook(String bookName, Date publicationDate, List<String> authors, String token) {
+
+    }
+
+    public void deleteBook(int bookCopyId, String token) {
+
+    }
+
+    public boolean markAsBlocked(int memberId, String token) {
+
+    }
+
+    public void issueBook(int bookCopyId, int memberId, String token) {
+
+    }
+
+    public void submitBook(int bookCopyId, int memberId, String token) {
+
+    }
+
+    public Member getBorrowerOfBook(int bookId, String token) {
+
+    }
+
+    public List<BookCopy> getBorrowedBooksByMember(int memberId, String token) {
+
+    }
+}
